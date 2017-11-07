be0e6fc99e5188b643b551cef0e8f979bfe5bba7
yvanwangl <1012305328@qq.com>
2017-11-07 22:40:14 +0800

    modify cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import 'reflect-metadata';
 import * as Koa from 'koa';
-import * as Router from 'koa-router';
+import * as cors from '@koa/cors';
 import * as koaBody from 'koa-body';
 import controller from './controller';
 
 const app = new Koa();
-const router = new Router();
 
-app.use(async (ctx, next)=>{
+app.use(cors({
+    origin: 'http://localhost:3000',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+    credentials: true,
+}));
+
+app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url} ${ctx.request.path}`);
     let startTime = new Date().getTime(),
         execTime;
     await next();
-    execTime = new Date().getTime()-startTime;
+    execTime = new Date().getTime() - startTime;
     ctx.response.set('X-Response-Time', execTime);
 });
 app.use(koaBody());
@@ -20,8 +25,5 @@ app.use(koaBody());
 //controller
 app.use(controller());
 
-
-app.use(router.routes());
-
-app.listen(3000);
-console.log('app started at port 3000...');
+app.listen(4000);
+console.log('app started at port 4000...');
